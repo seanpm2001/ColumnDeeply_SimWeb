ccbf516ffc7ebb3f641dceaaf44ed47af526d529
columndeeply <columndeeply@proton.me>
2023-03-12 10:33:28 +0100

    Add basic WebView with back button support

--- app/src/main/java/com/example/simweb/MainActivity.kt
+++ app/src/main/java/com/example/simweb/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.simweb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myWebView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Hide ActionBar
+        supportActionBar?.hide();
+
+        // Create WebView
+        myWebView = WebView(this)
+        setContentView(myWebView)
+
+        // Enable JS and set URL handler
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()
+
+        // Load website
+        myWebView.loadUrl("https://example.org")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
